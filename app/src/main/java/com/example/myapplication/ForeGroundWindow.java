c1005d66ad8baf603aad99645270c7ab2ebc09fd
Nebil Shibeshi <narega678@gmail>
2024-08-13 20:52:04 +0300

    make overlay fullscreen

--- app/src/main/java/com/example/myapplication/ForeGroundWindow.java
+++ app/src/main/java/com/example/myapplication/ForeGroundWindow.java
@@ -24,11 +24,10 @@ public class ForeGroundWindow {
     public ForeGroundWindow(Context context) {
         this.context = context;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            // set the layout parameters of the window
+            // Set the layout parameters of the window
             mParams = new WindowManager.LayoutParams(
-                    // Shrink the window to wrap the content rather
-                    // than filling the screen
-                    WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+                    // Fill the screen
+                    WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT,
                     // Display it on top of other application windows
                     WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY,
                     // Don't let it grab the input focus
@@ -37,11 +36,11 @@ public class ForeGroundWindow {
                     // through any transparent parts
                     PixelFormat.TRANSLUCENT);
         }
-        // getting a LayoutInflater
+        // Getting a LayoutInflater
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        // inflating the view with the custom layout we created
+        // Inflating the view with the custom layout we created
         mView = layoutInflater.inflate(R.layout.popup_layout, null);
-        // set onClickListener on the remove button, which removes
+        // Set onClickListener on the remove button, which removes
         // the view from the window
         mView.findViewById(R.id.window_close).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,17 +48,14 @@ public class ForeGroundWindow {
                 close();
             }
         });
-        // Define the position of the
-        // window within the screen
-        mParams.gravity = Gravity.BOTTOM;
+        // Define the position of the window within the screen
+        mParams.gravity = Gravity.CENTER; // Center the view
         mWindowManager = (WindowManager) context.getSystemService(WINDOW_SERVICE);
     }
 
     public void open() {
-
         try {
-            // check if the view is already
-            // inflated or present in the window
+            // Check if the view is already inflated or present in the window
             if (mView.getWindowToken() == null) {
                 if (mView.getParent() == null) {
                     mWindowManager.addView(mView, mParams);
@@ -71,17 +67,13 @@ public class ForeGroundWindow {
     }
 
     public void close() {
-
         try {
-            // remove the view from the window
+            // Remove the view from the window
             ((WindowManager) context.getSystemService(WINDOW_SERVICE)).removeView(mView);
-            // invalidate the view
+            // Invalidate the view
             mView.invalidate();
-            // remove all views
+            // Remove all views
             ((ViewGroup) mView.getParent()).removeAllViews();
-
-            // the above steps are necessary when you are adding and removing
-            // the view simultaneously, it might give some exceptions
         } catch (Exception e) {
             Log.d("Error2", e.toString());
         }
